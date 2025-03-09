c046ecf36350a2acb8a86f61e084c4c58a62a935
Sujal Dhaduk <167689197+sujals170@users.noreply.github.com>
2025-03-09 19:29:13 +0530

    Delete Bright Byte directory

--- Bright Byte/server/utils/generate.js
+++
@@ -1,6 +0,0 @@
-const generateOTP = () => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  return otp;
-};
-
-module.exports = { generateOTP };
--- Bright Byte/server/utils/jwtGenerator.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-  return token;
-};
-
-module.exports = { generateToken };
