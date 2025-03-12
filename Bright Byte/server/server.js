da4ac6c59c55c97be80e3571f330dff2434ba7c9
Sujal Dhaduk <167689197+sujals170@users.noreply.github.com>
2025-03-12 17:01:05 +0530

    Update server.js

--- Bright Byte/server/server.js
+++ Bright Byte/server/server.js
@@ -24,7 +24,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "bright-byte.vercel.app",
+    origin: "bright-byte-n1m6.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
